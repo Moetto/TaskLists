b67b81731d47c22d0e53577a2db3f565bf02a881
Antti Möttönen <antti.moettoenen@gmail.com>
2016-03-13 19:29:55 +0200

    Implement ListView in fragment
    TODOTaskListsFragment now shows static items inside ListView and listens to clicks

--- app/src/main/java/t3waii/tasklists/TODOTasksFragment.java
+++ app/src/main/java/t3waii/tasklists/TODOTasksFragment.java
@@ -2,23 +2,39 @@ package t3waii.tasklists;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by moetto on 3/11/16.
  */
-public class TODOTasksFragment extends Fragment {
+public class TODOTasksFragment extends ListFragment implements AdapterView.OnItemClickListener{
+    public static final String TAG = "TODOTasksFragment";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(
-                R.layout.todo_tasks_list, container, false);
+        View rootView = inflater.inflate(R.layout.todo_tasks_list, container, false);
         Bundle args = getArguments();
-        return rootView;
 
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter =  new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, new String[]{"1", "2","3","4","5","6","7","8"});
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "Halp, I've been clicked");
     }
 }
