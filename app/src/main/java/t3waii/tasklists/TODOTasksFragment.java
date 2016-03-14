3aaaa7479d84db1a48580e35b0a560262c49e556
Antti Möttönen <antti.moettoenen@gmail.com>
2016-03-14 09:51:02 +0200

    Add complex items to tasklist
    Changed tasklists Adapter to custom adapter allowing using custom Views as
    list items. List items can now handle multiple onClickListeners and have
    arbitary elements.

--- app/src/main/java/t3waii/tasklists/TODOTasksFragment.java
+++ app/src/main/java/t3waii/tasklists/TODOTasksFragment.java
@@ -7,13 +7,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
 
 /**
  * Created by moetto on 3/11/16.
  */
-public class TODOTasksFragment extends ListFragment implements AdapterView.OnItemClickListener{
+public class TODOTasksFragment extends ListFragment{
     public static final String TAG = "TODOTasksFragment";
 
     @Nullable
@@ -28,13 +29,42 @@ public class TODOTasksFragment extends ListFragment implements AdapterView.OnIte
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter =  new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, new String[]{"1", "2","3","4","5","6","7","8"});
-        setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
-    }
 
+        //ArrayAdapter adapter =  new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, new String[]{"1", "2","3","4","5","6","7","8"});
+        setListAdapter(taskListAdapter);
+        //getListView().setOnItemClickListener(this);
+    }
+    /*
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Log.d(TAG, "Halp, I've been clicked");
     }
+    */
+    BaseAdapter taskListAdapter = new BaseAdapter(){
+        private final String[] dummyTasks = new String[]{"a","b","c","d"};
+        @Override
+        public int getCount() {
+            return dummyTasks.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return dummyTasks[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.complex_task, null);
+            }
+            TextView textView = (TextView)convertView.findViewById(R.id.complex_text);
+            textView.setText(dummyTasks[position]);
+            return convertView;
+        }
+    };
 }
