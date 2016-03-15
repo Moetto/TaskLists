93077b218fc52d881bb859d53205aebc9fa0379e
Antti Möttönen <antti.moettoenen@gmail.com>
2016-03-15 09:57:29 +0200

    Make new task icon start blank activity

--- app/src/main/java/t3waii/tasklists/MainActivity.java
+++ app/src/main/java/t3waii/tasklists/MainActivity.java
@@ -1,6 +1,7 @@
 package t3waii.tasklists;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -35,15 +36,14 @@ public class MainActivity extends AppCompatActivity {
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
 
-        tabs = (TabLayout)findViewById(R.id.tabs);
+        tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(pager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, NewTask.class));
             }
         });
     }
