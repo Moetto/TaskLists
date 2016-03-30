74ee1eb8a3f5d2f02c5f03437c44b18dde5a9455
Matti Pulkkinen <mattpulk@gmail.com>
2016-03-30 13:09:39 +0300

    Add group menu items

--- app/src/main/java/t3waii/tasklists/MainActivity.java
+++ app/src/main/java/t3waii/tasklists/MainActivity.java
@@ -1,5 +1,7 @@
 package t3waii.tasklists;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,13 +10,17 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int TAB_COUNT = 4;
+    private Menu menu;
     FragmentPagerAdapter pagerAdapter;
     ViewPager pager;
     TabLayout tabs;
@@ -48,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
         return true;
     }
 
@@ -59,11 +66,87 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_settings:
+                //TODO: open settings
+                return true;
+            case R.id.dialog_newgroup_settings:
+                createNewGroup();
+                return true;
+            case R.id.dialog_managegroup_settings:
+                //TODO: open edit group dialog
+                return true;
+            case R.id.dialog_leavegroup_settings:
+                leaveCurrentGroup();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // When user is in group, hide new group menu item and show manage group and leave group.
+    private void setMainMenuGroupItemsVisibility(boolean isInGroup) {
+        MenuItem item = (MenuItem) this.menu.findItem(R.id.dialog_newgroup_settings);
+        item.setVisible(!isInGroup);
+        item = (MenuItem) this.menu.findItem(R.id.dialog_managegroup_settings);
+        item.setVisible(isInGroup);
+        item = (MenuItem) this.menu.findItem(R.id.dialog_leavegroup_settings);
+        item.setVisible(isInGroup);
+    }
+
+    // Take new group name, show/hide menu elements and register new group
+    private void leaveCurrentGroup() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View promptView = layoutInflater.inflate(R.layout.group_confirm_leave, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // set group_new.xml to be the layout file of the alertdialog builder
+        alertDialogBuilder.setView(promptView);
+        // setup a dialog window
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // get user input and set it to result
+                        //TODO: unregister self from current group
+                        setMainMenuGroupItemsVisibility(false);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        // create an alert dialog
+        AlertDialog alertD = alertDialogBuilder.create();
+        alertD.show();
+    }
+
+    // Take new group name, show/hide menu elements and register new group
+    private void createNewGroup() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View promptView = layoutInflater.inflate(R.layout.group_new, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // set group_new.xml to be the layout file of the alertdialog builder
+        alertDialogBuilder.setView(promptView);
+        final EditText input = (EditText) promptView.findViewById(R.id.newGroupName);
+        // setup a dialog window
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // get user input and set it to result
+                        Log.d("test", "value:" + input.getText());
+                        setMainMenuGroupItemsVisibility(true);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        // create an alert dialog
+        AlertDialog alertD = alertDialogBuilder.create();
+        alertD.show();
     }
 
     public static class TabAdapter extends FragmentPagerAdapter {
